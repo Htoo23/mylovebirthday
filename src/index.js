d753ee0d9bdee5712f4d401ab4f9316fdba5a77d
Htoo Aung Lin <59634121+Htoo23@users.noreply.github.com>
2024-02-12 09:57:44 +0630

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const rootElement=document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
+    <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+  </StrictMode>,
+  rootElement
+)
